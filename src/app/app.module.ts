dad4bd16aea41cda11a7261cfb3dd4eecbe76269
Cumhur Düzlü <cumhurduzlu@Cumhurs-MacBook-Pro.local>
2018-02-11 15:54:31 +0100

    UI template changed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { CommonModule } from '@angular/common';
 
-
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { NavMenuComponent } from './navmenu/navmenu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule, 
+        HttpModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: '**', redirectTo: 'home' }
+        ])
   ],
   providers: [],
   bootstrap: [AppComponent]
